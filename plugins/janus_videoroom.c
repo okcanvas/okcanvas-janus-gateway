fa05673e9114ed77b19f58e69a0892380c06584d
okcanvas <okcanvas@name.com>
2026-04-28 09:19:45 +0900

    first

--- plugins/janus_videoroom.c
+++ plugins/janus_videoroom.c
@@ -5598,14 +5598,6 @@ void janus_videoroom_incoming_rtcp(janus_plugin_session *handle, janus_plugin_rt
 			session->peer_bitrate = bitrate;
 			gateway->send_remb(handle, session->bitrate ? session->bitrate : 10000000);
 			*/
-
-			/* A subscriber sent REMB. Forward it to the publisher, if available. */
-			if(s->feed) {
-				janus_videoroom_publisher *p = s->feed;
-				if(p && p->session && p->session->handle) {
-					gateway->send_remb(p->session->handle, bitrate);
-				}
-			}
 		}
 		janus_refcount_decrease_nodebug(&s->ref);
 	}
